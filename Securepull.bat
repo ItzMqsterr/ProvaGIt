2dc6f649eaa4d3e72712eb1e147f2ca9eb5bfbd7
Notkyx <godgamers3566@gmail.com>
2024-12-14 00:42:10 -0700

    ciao

--- Securepull.bat
+++ Securepull.bat
@@ -2,7 +2,7 @@
 git add .
 git commit -m "Pull-1"
 git fetch origin master
-git pull -X theirs --no-edit origin master
+git pull -X theirs --no-commit
 git add .
 git commit -m "Secure-pull-2"
 pause
